a691d4827e1b11a7dd9823dc49dc7f53112c6088
park <carhyunjun@naver.com>
2024-04-26 12:29:32 +0900

    upload

--- flaskbook/apps/app.py
+++ flaskbook/apps/app.py
@@ -4,36 +4,44 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from apps.config import config
+from flask_login import LoginManager
 
-def create_app(config_key):
-    app = Flask(__name__)
-    app.config.from_object(config[config_key])
 
 
 db = SQLAlchemy()
-
 csrf = CSRFProtect()
+login_manager = LoginManager()
+login_manager.login_view = "auth.signup"
+login_manager.login_message = ""
 
-def create_app():
+
+
+def create_app(config_key):
 
     app = Flask(__name__)
+    app.config.from_object(config[config_key])
 
-    app.config.from_mapping(
+    '''app.config.from_mapping(
         SECRET_KEY = "2fqfaf5286478",
         SQLALCHEMY_DATABASE_URI = f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
 
         SQLALCHEMY_ECHO = True,
         WTF_CSRF_SECRET_KEY = "4a6wtatqr64"
-    )
+    )'''
 
     csrf.init_app(app)
     db.init_app(app)
     Migrate(app,db)
+    login_manager.init_app(app)
 
     from apps.crud import views as crud_views
 
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
+
+    from apps.auth import views as auth_views
+
+    app.register_blueprint(auth_views.auth, url_prefix ="/auth")
 
     return app
 
--- flaskbook/apps/crud/views.py
+++ flaskbook/apps/crud/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template,redirect,url_for
 from apps.app import db
 from apps.crud.models import User
 from apps.crud.forms import UserForm
+from flask_login import login_required
 crud = Blueprint(
     "crud",
     __name__,
@@ -11,15 +12,18 @@ crud = Blueprint(
 )
 
 @crud.route("/")
+@login_required
 def index():
     return render_template("crud/index.html")
 
 @crud.route("/sql")
+@login_required
 def sql():
     db.session.query(User).all()
     return "콘솔 로그를 확인해 주세요"
 
 @crud.route("/users")
+@login_required
 def users():
     users = User.query.all()
     return render_template("crud/index.html",users = users)
@@ -27,6 +31,7 @@ def users():
 
 
 @crud.route("/users/new",methods = ["GET","POST"])
+@login_required
 def create_user() :
     form = UserForm()
     if form.validate_on_submit():
@@ -41,6 +46,7 @@ def create_user() :
     return render_template("crud/create.html", form=form)
 
 @crud.route("/users/<user_id>",methods=["GET","POST"])
+@login_required
 def edit_user(user_id):
     form = UserForm()
 
@@ -56,6 +62,7 @@ def edit_user(user_id):
     return render_template("crud/edit.html",user=user, form=form)
 
 @crud.route("/user/<user_id>/delete",methods=["POST"])
+@login_required
 def delete_user(user_id):
     user = User.query.filter_by(id=user_id).first()
     db.session.delete(user)
